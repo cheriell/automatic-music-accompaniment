bff6e66a01315dcc1468aaabe4f40aaed38f8392
cheriell <32418246+cheriell@users.noreply.github.com>
2018-08-22 14:53:34 +0100

    Update load_data_to_files.py

--- code/load_data_to_files.py
+++ code/load_data_to_files.py
@@ -3,13 +3,16 @@
 Created on Fri Jul 27 13:12:45 2018
 
 @author: fj123
+@description: This script encode all the midi music in the collected dataset into the 
+desined data representation. The encoded data representations will be saved in .npy format
+under the same directory of the midi file.
 """
 
 import utils
 import numpy as np
 import os
 
-
+# load all the data paths in the midi dataset.
 path = '..\\midis\\'
 try:
     data_paths = [os.path.join(path, o) \
@@ -19,6 +22,8 @@ except OSError as e:
     print('Error: Invalid datapath!!!')
 
 
+# convert all the midis into data representation and save as .npy file.
+# the encoded data file will be monophinic and wrapped within one octave.
 for data_path in data_paths:
     midi_files = utils.get_file_paths(data_path)
     
